8ca91e7d02c12b501442f4b141408f1843a2ebdb
ExposedCat <artem13.prokop@gmail.com>
2022-10-03 00:18:15 +0200

    🔧 [TEST] Improve testing engine

--- PRP/test.bash
+++ PRP/test.bash
@@ -2,25 +2,30 @@
 
 # Compile
 echo ">>> Compiling"
-clang -pedantic -Wall -Werror -std=c99 -O2 -g main.c -o main
+clang -pedantic -Wall -Werror -std=c99 -O2 -lm -g main.c -o main
 
 # Test
 echo ">>> Testing"
 mkdir -p out
 TESTS_NUMBER=$(command ls -1q data/pub*.in | wc -l)
 echo ">>> Found $TESTS_NUMBER tests"
-for ((i=1;i<=TESTS_NUMBER;i++)); do
-    echo ">>>> Running test $i";
-    ./main < data/pub0$i.in 2> out/my-0$i.err 1> out/my-0$i.out;
+
+i=1
+cd ./data
+for file in *.in; do
+    echo ">>>> Running test $i"
+    ID=$(echo $file | sed 's/pub\(.*\)\..*/\1/')
+    ../main < ./pub$ID.in 2> ../out/my$ID.err 1> ../out/my$ID.out
     # Show difference
-    echo ">>> Result:";
-    diff data/pub0$i.out out/my-0$i.out\
+    echo ">>> Result:"
+    diff ./pub$ID.out ../out/my$ID.out\
         --old-line-format=$'- %L'\
         --new-line-format='+ %L'\
-        --unchanged-line-format='' && echo "OUT OK";
+        --unchanged-line-format='' && echo "OUT OK"
         
-    diff data/pub0$i.err out/my-0$i.err\
+    diff ./pub$ID.err ../out/my$ID.err\
         --old-line-format=$'- %L'\
         --new-line-format='+ %L'\
-        --unchanged-line-format='' && echo "ERR OK";
+        --unchanged-line-format='' && echo "ERR OK"
+    ((i=i+1))
 done
